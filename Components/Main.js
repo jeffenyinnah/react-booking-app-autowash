8aada06a1c3195ffdfaaf1d92496434eb3a94fa9
jeffenyinnah <jeffenyinnah@gmail.com>
2020-08-27 03:07:16 +0200

    1 file modified

--- Components/Main.js
+++ Components/Main.js
@@ -8,6 +8,9 @@ class Main extends Component {
     return (
       <div>
         <header>
+          <img className="card-img-top rounded-circle w200" src="https://firebasestorage.googleapis.com/v0/b/auto-washimatic.appspot.com/o/logo.jpg?alt=media&token=573152dc-6db9-434c-990b-48bef03638c0"
+              alt="Logo Image"
+            />
           <h3>Auto-Washimatic</h3>
           <NavLink to="/login">Login/Sign Up</NavLink>
         </header>        
